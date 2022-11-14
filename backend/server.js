1e5d5abca986ff322d8645616bb5939735a5420e
PATEL YASH ANILKUMAR <107877404+patelYashA@users.noreply.github.com>
2022-11-14 12:54:00 +0530

    first commit

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const app = require("express")()
+const server = require("http").createServer(app);
+const io = require("socket.io")(server, {
+    cors: {
+        origin: "*"
+    }
+})
+const cors = require('cors');
+app.use(cors());
+
+io.on("connection", (socket) => {
+    console.log(socket);
+
+    socket.on("chat", (payload) => {
+        console.log(payload);
+        io.emit("chat", payload)
+    })
+})
+
+server.listen(5000, () => {
+    console.log("server is listening at port 5000");
+})
